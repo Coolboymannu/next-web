442afb222b11fcfe04fdb47326e1ce06c7827522
Coolboymannu <coolboymannu.coder@gmail.com>
2024-01-15 10:44:51 +0530

    editing

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-// next.config.js
-module.exports = {
-    // Other configurations...
-  
-    // Enable static HTML export
-    async generateStaticParams() {
-      // Return an object representing the paths and pages to be exported
-      return {
-        '/': { page: '/' },
-        // Add other pages as needed
-      };
-    },
-  };
-  
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
