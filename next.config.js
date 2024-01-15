1c0625bc2998b977272c689e0a0392322870c32e
Coolboymannu <coolboymannu.coder@gmail.com>
2024-01-15 10:36:22 +0530

    changing file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    // Other configurations...
+  
+    // Enable static HTML export
+    async generateStaticParams() {
+      // Return an object representing the paths and pages to be exported
+      return {
+        '/': { page: '/' },
+        // Add other pages as needed
+      };
+    },
+  };
+  
