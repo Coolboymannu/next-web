528ebfe7db2475673503f50e23f416fd521f00a2
Coolboymannu <coolboymannu.coder@gmail.com>
2024-01-15 11:01:25 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-
+// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+    output: 'export',
+    // Other configurations...
+  };
+  
+  module.exports = nextConfig;
+  
